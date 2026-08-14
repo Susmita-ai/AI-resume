a2a11df43e0337d5dabd2dac0c9d7e652528b7e3
Susmita-ai <yadavsusmita623@gmail.com>
2026-08-14 16:20:26 +0530

    Update predictor.py

--- utils/predictor.py
+++ utils/predictor.py
@@ -2,7 +2,9 @@ import os
 import joblib
 import pandas as pd
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
 
 MODEL_PATH = os.path.join(
     BASE_DIR,
@@ -23,18 +25,11 @@ label_encoder = joblib.load(ENCODER_PATH)
 
 
 def predict_job_role(features):
-    """
-    features: dict with keys matching the columns used in train_model.py:
-        Skills, Experience (Years), Education, Certifications,
-        Salary Expectation ($), Projects Count, AI Score (0-100)
-    """
 
     df = pd.DataFrame([features])
 
     encoded_prediction = model.predict(df)[0]
 
-    job_role = label_encoder.inverse_transform(
+    return label_encoder.inverse_transform(
         [encoded_prediction]
     )[0]
-
-    return job_role
